093b059798aacd22822cd6d30e9945512f1c339d
klouds <adalwolfad@gmail.com>
2024-01-29 22:52:54 +0100

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -16,39 +16,36 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              Déjame  <span className="purple"> Déjame  </span> 
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              Me enamoré de la programación hace un par de años y he tomado la decisión de dedicarme profesionalmente a ella.
               <br />
-              <br />I am used to work with
+              <br />Estoy familiarizado con
               <i>
-                <b className="purple"> Python, Javascript and SQL . </b>
+                <b className="purple"> Python, Javascript y SQL . </b>
               </i>
               <br />
               <br />
-              My field of Interest's are building new &nbsp;
+             Me apasiona construir con &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                <b className="purple"> Nuevas tecnologías y productos web, </b> así como explorar áreas relacionadas con{" "}
                 <b className="purple">
-                  Cybersecurity and Artificial Intelligence.
+                  la ciberseguridad y la inteligencia artificial.
                 </b>
               </i>
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
+            disfruto desarrollar productos con <b className="purple">Node.js</b> and
               <i>
                 <b className="purple">
                   {" "}
-                  Modern Javascript Library and Frameworks
+                  bibliotecas y frameworks modernos de Javascript
                 </b>
               </i>
-              &nbsp; like
+              &nbsp; como
               <i>
-                <b className="purple"> React.js and Next.js</b>
+                <b className="purple"> React.js y Next.js</b>
               </i>
             </p>
           </Col>
@@ -60,9 +57,9 @@ function Home2() {
         </Row>
         <Row>
           <Col md={12} className="home-about-social">
-            <h1>FIND ME ON</h1>
+            <h1>ENCUÉNTRAME EN </h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+             Siéntete libre de <span className="purple"> contactarme </span>with me
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
