2899827043c50bbfc437469a4b35a4ece35bfdfe
klouds <adalwolfad@gmail.com>
2024-02-08 20:28:57 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -13,10 +13,10 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          Proyectos <strong className="purple"> Recientes </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
+         Algunos proyectos en los que he trabajado ultimamente
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
