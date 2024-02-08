bc4d6c2f0744e4a5b0885561ac41a7ebd2ecf342
klouds <adalwolfad@gmail.com>
2024-02-08 20:24:32 +0100

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -37,7 +37,7 @@ function Projects() {
               isBlog={false}
               title="BuberBreakfast"
               description="CRUD REST API from scratch using .NET 6. The backend system supports Creating, Reading, Updating and Deleting breakfasts."
-              ghLink="https://github.com/klouds27"
+              ghLink="https://github.com/klouds27/buberBreakfast"
               
             />
           </Col>
@@ -51,6 +51,17 @@ function Projects() {
               description="This project allowed me to implement cutting-edge technologies and best practices, including Next.js 13, React.js, Tailwind CSS, TypeScript, and a mobile-first approach. The result is a fully responsive, feature-rich travel company website that showcases my skills in UI/UX design and web development."
               ghLink="https://github.com/klouds27/travel_app"
               demoLink="hilink-alpha.vercel.app"              
+            />
+          </Col>
+                
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={MintyHost}
+              isBlog={false}
+              title="Minty Host Apartments Showcase"
+              description="Este proyecto me permitió sumergirme en Vue.js, explorando la creación de componentes reutilizables y diseñando una experiencia de usuario intuitiva."
+              ghLink="https://github.com/klouds27/minty-casop"
+              demoLink="https://minty-casop.vercel.app"              
             />
           </Col>
 
